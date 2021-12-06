8ee9f97e3a01aa28af7a2dccfcbb650986537b77
l33th <l33th@protonmail.ch>
2021-12-06 19:43:32 +0200

    full angular list project

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let angularList = angular.module('myList', []);
 
 angularList.controller('myListController', ($scope) => {
-	$scope.items = ['AngularJS', 'ReactJS', 'UnderscoreJS'];
+	$scope.items = ['AngularJS', 'ReactJS', 'VueJS'];
 	$scope.newItem = '';
+	$scope.pushItem = () => {
+		if ($scope.newItem != '') {
+			$scope.items.push($scope.newItem);
+			$scope.newItem = '';
+		}
+	};
 });
